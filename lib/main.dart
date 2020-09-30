1f222747ac1d573c94a2a9e659591a2a2e24eeec
vijay gohel <gohil5898@gmail.com>
2020-09-30 13:15:35 +0530

    done padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
+  runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
+        home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(backgroundColor: Colors.red, title: Text('Dicee')),
+      body: DiceApp(),
+    )),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DiceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Image.asset('images/dice1.png'),
+          )),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Image.asset('images/dice1.png'),
+          ))
+        ],
+      ),
+    );
   }
 }
